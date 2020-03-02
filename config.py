ea5683c5e488e058fb39298b97383a278835a717
yanghuiyu <760997646@qq.com>
2020-03-02 13:02:53 +0800

    加入竖排文字识别

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ father_path=os.path.abspath(os.path.dirname(filt_path)+os.path.sep+".")
 GPU_ID = 0
 
 #psenet相关
-pse_long_size = 960
+pse_long_size = 1280
 pse_model_type  = "mobilenetv2"
 pse_scale = 1
 
