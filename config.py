15b29e428a027b58574b130db886c9bb3d7a661d
yanghuiyu <760997646@qq.com>
2020-07-29 19:22:22 +0800

    onnx

--- config.py
+++ config.py
@@ -5,13 +5,16 @@ father_path = os.path.abspath(os.path.dirname(filt_path) + os.path.sep + ".")
 
 
 dbnet_short_size = 960
-
+crnn_type = "full"
 
 model_path = os.path.join(father_path, "models/dbnet.onnx")
 
 # crnn相关
 nh = 256
 
+# if crnn_type == "full":
+#     crnn_model_path = os.path.join(father_path, "models/crnn_lstm.onnx")
+# elif crnn_type == "lite":
 crnn_model_path = os.path.join(father_path, "models/crnn_lite_lstm.onnx")
 
 
