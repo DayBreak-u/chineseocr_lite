30446f52c922ec23f77e35f0e396ddc0325d4853
yanghuiyu <760997646@qq.com>
2020-08-14 17:02:04 +0800

    update model

--- backend/webInterface/tr_run.py
+++ backend/webInterface/tr_run.py
@@ -18,12 +18,16 @@ import logging
 logger = logging.getLogger(log.LOGGER_ROOT_NAME + '.' +__name__)
 
 ocrhandle = OcrHandle()
+request_time = {}
+now_time = time.strftime("%Y-%m-%d", time.localtime(time.time()))
+from config import max_post_time ,dbnet_max_size,white_ips
+
 
 class TrRun(tornado.web.RequestHandler):
     '''
     使用 tr 的 run 方法
     '''
-
+        
     def get(self):
         self.set_status(404)
         self.write("404 : Please use POST")
@@ -38,8 +42,9 @@ class TrRun(tornado.web.RequestHandler):
 
         '''
         start_time = time.time()
-        MAX_SIZE = 1600
-
+        short_size = 960
+        global now_time
+        global request_time
         img_up = self.request.files.get('file', None)
         img_b64 = self.get_argument('img', None)
         compress_size = self.get_argument('compress', None)
@@ -79,54 +84,88 @@ class TrRun(tornado.web.RequestHandler):
             self.finish(error_log)
             return
         img = img.convert("RGB")
+        
+        time_now = time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+        time_day = time.strftime("%Y-%m-%d", time.localtime(time.time()))
+        if time_day != now_time:
+            now_time = time_day
+            request_time = {}
+        img.save("../web_imgs/{}.jpg".format(time_now))
+        
 
         '''
         是否开启图片压缩
-        默认为1600px
+        默认为960px
         值为 0 时表示不开启压缩
         非 0 时则压缩到该值的大小
         '''
+        res = []
+        do_det = True
+        remote_ip_now = self.request.remote_ip
+        if remote_ip_now not in request_time :
+            request_time[remote_ip_now] = 1 
+        elif request_time[remote_ip_now] > max_post_time -1 and remote_ip_now not in white_ips  :
+            res.append("今天访问次数超过{}次！".format(max_post_time))
+            do_det = False
+        else:
+            request_time[remote_ip_now] += 1
+
         if compress_size is not None:
             try:
                 compress_size = int(compress_size)
             except ValueError as ex:
-                logger.error(exc_info=True)
-                self.finish(json.dumps({'code': 400, 'msg': 'compress参数类型有误，只能是int类型'}, cls=NpEncoder))
-                return
+                # logger.error(exc_info=True)
+                res.append("短边尺寸参数类型有误，只能是int类型")
+                do_det = False
+                # self.finish(json.dumps({'code': 400, 'msg': 'compress参数类型有误，只能是int类型'}, cls=NpEncoder))
+                # return
 
-            if compress_size < 1:
-                MAX_SIZE = max(img.height, img.width)
-            else:
-                MAX_SIZE = compress_size
+            short_size = compress_size
+            if short_size < 64:
+                res.append("短边尺寸过小，请调整短边尺寸")
+                do_det = False
 
-        if img.height > MAX_SIZE or img.width > MAX_SIZE:
-            scale = max(img.height / MAX_SIZE, img.width / MAX_SIZE)
+            short_size = 32 * (short_size//32)
 
-            new_width = int(img.width / scale + 0.5)
-            new_height = int(img.height / scale + 0.5)
-            img = img.resize((new_width, new_height), Image.BICUBIC)
 
-        res = ocrhandle.text_predict(img)
+        img_w, img_h = img.size
+        if max(img_w, img_h) * (short_size * 1.0 / min(img_w, img_h)) > dbnet_max_size:
+            # logger.error(exc_info=True)
+            res.append("图片reize后长边过长，请调整短边尺寸")
+            do_det = False
+            # self.finish(json.dumps({'code': 400, 'msg': '图片reize后长边过长，请调整短边尺寸'}, cls=NpEncoder))
+            # return
 
-        img_detected = img.copy()
-        img_draw = ImageDraw.Draw(img_detected)
-        colors = ['red', 'green', 'blue', "purple"]
 
-        for i, r in enumerate(res):
-            rect, txt, confidence = r
-            x1,y1,x2,y2,x3,y3,x4,y4 = rect.reshape(-1)
-            for xy in [(x1, y1, x2, y2), (x2, y2, x3, y3 ), (x3 , y3 , x4, y4), (x4, y4, x1, y1)]:
-                img_draw.line(xy=xy, fill=colors[i % len(colors)], width=2)
+        if do_det:
 
-        output_buffer = BytesIO()
-        img_detected.save(output_buffer, format='JPEG')
-        byte_data = output_buffer.getvalue()
-        img_detected_b64 = base64.b64encode(byte_data).decode('utf8')
+            res = ocrhandle.text_predict(img,short_size)
+
+            img_detected = img.copy()
+            img_draw = ImageDraw.Draw(img_detected)
+            colors = ['red', 'green', 'blue', "purple"]
+
+            for i, r in enumerate(res):
+                rect, txt, confidence = r
+                x1,y1,x2,y2,x3,y3,x4,y4 = rect.reshape(-1)
+                for xy in [(x1, y1, x2, y2), (x2, y2, x3, y3 ), (x3 , y3 , x4, y4), (x4, y4, x1, y1)]:
+                    img_draw.line(xy=xy, fill=colors[i % len(colors)], width=2)
+
+            output_buffer = BytesIO()
+            img_detected.save(output_buffer, format='JPEG')
+            byte_data = output_buffer.getvalue()
+            img_detected_b64 = base64.b64encode(byte_data).decode('utf8')
+        
+        else:
+            output_buffer = BytesIO()
+            img.save(output_buffer, format='JPEG') 
+            byte_data = output_buffer.getvalue()
+            img_detected_b64 = base64.b64encode(byte_data).decode('utf8')
 
         log_info = {
-            'ip': self.request.host,
+            'ip': self.request.remote_ip,
             'return': res,
-            'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            'time': time_now
         }
         logger.info(json.dumps(log_info, cls=NpEncoder))
         self.finish(json.dumps(
--- crnn/CRNN.py
+++ crnn/CRNN.py
@@ -10,17 +10,25 @@ from .util import strLabelConverter, resizeNormalize
 
 converter = strLabelConverter(''.join(alphabet))
 
+def softmax(x):
+    x_row_max = x.max(axis=-1)
+    x_row_max = x_row_max.reshape(list(x.shape)[:-1]+[1])
+    x = x - x_row_max
+    x_exp = np.exp(x)
+    x_exp_row_sum = x_exp.sum(axis=-1).reshape(list(x.shape)[:-1]+[1])
+    softmax = x_exp / x_exp_row_sum
+    return softmax
+
 
 class CRNNHandle:
     def __init__(self, model_path):
 
         self.sess = rt.InferenceSession(model_path)
 
-    def predict(self, im):
+    def predict(self, image):
         """
         预测
         """
-        image = im.convert('L')
         scale = image.size[1] * 1.0 / 32
         w = image.size[0] / scale
         w = int(w)
@@ -38,12 +46,42 @@ class CRNNHandle:
 
         length  = preds.shape[0]
         preds = preds.reshape(length,-1)
-        # preds = 1/(1+np.exp(-1 * preds))
 
-        # temp_fist = preds[:,0]
-        # temp_fist[temp_fist>0.001] = 0.001
-        #
-        # preds[:,0 ] =temp_fist
+        preds = np.argmax(preds,axis=1)
+
+        preds = preds.reshape(-1)
+
+
+        sim_pred = converter.decode(preds, length, raw=False)
+
+        return sim_pred
+
+
+
+    def predict_rbg(self, im):
+        """
+        预测
+        """
+        scale = im.size[1] * 1.0 / 32
+        w = im.size[0] / scale
+        w = int(w)
+
+        img = im.resize((w, 32), Image.BILINEAR)
+        img = np.array(img, dtype=np.float32)
+        img -= 127.5
+        img /= 127.5
+        image = img.transpose(2, 0, 1)
+        transformed_image = np.expand_dims(image, axis=0)
+
+        preds = self.sess.run(["out"], {"input": transformed_image.astype(np.float32)})
+
+        preds = preds[0]
+
+
+        length  = preds.shape[0]
+        preds = preds.reshape(length,-1)
+
+        # preds = softmax(preds)
 
 
         preds = np.argmax(preds,axis=1)
@@ -53,6 +91,7 @@ class CRNNHandle:
         sim_pred = converter.decode(preds, length, raw=False)
 
         return sim_pred
+
 
 
 if __name__ == "__main__":
