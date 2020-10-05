4dc60aed516c0d88611d6e4a4cd63f58d1c6f5a2
mayong <mayong@qianxin.com>
2020-10-05 10:58:56 +0800

    Optimize the main program to avoid wrong usage.

--- ncnn_project/ocr/src/main.cpp
+++ ncnn_project/ocr/src/main.cpp
@@ -2,12 +2,10 @@
 
 int main(int argc, char **argv) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s [model model path imagepath \n", argv[0]);
+        fprintf(stderr, "Usage: %s  /path/to/image/file  longsize \n", argv[0]);
+        std::cout << "eg: " << argv[0] << " e:\\temp\\test.jpg  560" << std::endl;
         return -1;
     }
-
-
-
 
     const char *imagepath = argv[1];
     cv::Mat im_bgr = cv::imread(imagepath, 1);
