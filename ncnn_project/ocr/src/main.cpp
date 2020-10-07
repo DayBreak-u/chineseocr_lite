757e40ee515a7c0023c8eb35d72fc301c3c48ee0
mayong <mayong@qianxin.com>
2020-10-08 07:41:12 +0800

    Rename the project to chocrlite.

--- ncnn_project/ocr/src/main.cpp
+++ ncnn_project/ocr/src/main.cpp
@@ -1,8 +1,9 @@
 #include "precomp.h"
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s  /path/to/image/file  longsize  models_dir \n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s  /path/to/image/file  longsize  verbose_mode models_dir \n", argv[0]);
         std::cout << "eg: " << argv[0] << " e:\\temp\\test.jpg  560" << std::endl;
+        std::cout << "eg: " << argv[0] << " e:\\temp\\test.jpg  560  1  e:\\ocr_models" << std::endl;
         std::cout << "Caution: please copy your models directory into  the same diirectory as your program: " <<argv[0] << std::endl;
 
         return -1;
@@ -11,8 +12,13 @@ int main(int argc, char **argv) {
 
     string strModelDir="models/";
     const int long_size = atoi(argv[2]);
-    if (argc == 4)
-        strModelDir = argv[3];
+    bool bVerbose = false;
+    if (argc >= 4)
+    {
+        bVerbose = atoi(argv[3]) > 0;
+    }
+    if (argc == 5)
+        strModelDir = argv[4];
     OCRENGINE_PTR pEngine =COL_InitOCREngine(strModelDir.c_str());
     if (!pEngine)
     {
@@ -21,13 +27,15 @@ int main(int argc, char **argv) {
     }
 
 
-    // COL_SetVerbose(pEngine, 1);
+    if(bVerbose)
+        COL_SetVerbose(pEngine, 1);
 
     char* pResult;
     int nSize = COL_Recognition(pEngine, argv[1], &pResult, long_size);
     if (nSize>= 0)
     {
-        printf("%s",pResult);
+        if(!bVerbose)
+            printf("%s",pResult);
 	    cout <<endl<<"total size: "<< nSize << " Bytes"<< endl;
     }
     else
