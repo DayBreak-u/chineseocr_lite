24dfe68f4ae7e7f292253f2e51e179b05c51e8db
mayong <mayong@qianxin.com>
2020-10-08 10:49:05 +0800

    detect the short_size automatically.

--- ncnn_project/ocr/src/main.cpp
+++ ncnn_project/ocr/src/main.cpp
@@ -15,9 +15,9 @@ typedef long long TIME_T;
 
 using namespace std;
 int main(int argc, char **argv) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s  /path/to/image/file  longsize  verbose_mode  use_gpu models_dir \n", argv[0]);
-        std::cout << "eg: " << argv[0] << " e:\\temp\\test.jpg  560" << std::endl;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s  /path/to/image/file  shortsize  verbose_mode  use_gpu models_dir \n", argv[0]);
+        std::cout << "eg (detect the short size automatically): " << argv[0] << " e:\\temp\\test.jpg " << std::endl;
         std::cout << "eg: " << argv[0] << " e:\\temp\\test.jpg  560  1  1 e:\\ocr_models" << std::endl;
         std::cout << "Caution: please copy your models directory into  the same diirectory as your program: " <<argv[0] << std::endl;
         std::cout << "By default: We don't use GPU. If you turn on GPU mode, it's possible there's no result." << std::endl;
@@ -26,7 +26,9 @@ int main(int argc, char **argv) {
 
 
     string strModelDir="models/";
-    const int long_size = atoi(argv[2]);
+    int short_size = 0;
+    if( argc >2)
+        short_size= atoi(argv[2]);
     bool bVerbose = false;
     bool bUseGPU = false;
     if (argc >= 4)
@@ -58,13 +60,14 @@ int main(int argc, char **argv) {
    
 
 
-    int ti;
-    ftime(&ts1);
-    int nSize = COL_Recognition(pEngine, argv[1], &pResult, long_size);
-    ftime(&ts2);
+    
+    //ftime(&ts1);
+    double nTotalTime = 0;
+    int nSize = COL_Recognition(pEngine, argv[1], &pResult, short_size,&nTotalTime);
+ /*   ftime(&ts2);
 
     t1 = (TIME_T)ts1.time * 1000 + ts1.millitm;
-    t2 = (TIME_T)ts2.time * 1000 + ts2.millitm;
+    t2 = (TIME_T)ts2.time * 1000 + ts2.millitm;*/
    
 
     if (nSize >= 0 && !bVerbose)
@@ -73,8 +76,8 @@ int main(int argc, char **argv) {
             printf("%s", pResult);
             cout << endl << "total size: " << nSize << " Bytes" << endl;
             
-            t1 = (t2 - t1) / 1000;
-            cout << "total time: " << t1 << " seconds" << endl;
+            //t1 = (t2 - t1) / 1000;
+            cout << "total time: " << nTotalTime << " seconds" << endl;
     }
         
     if(nSize <0)
