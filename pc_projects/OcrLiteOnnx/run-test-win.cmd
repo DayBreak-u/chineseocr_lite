5b91a14c2204edad543737555b693805b0b8aa4d
benjaminwan <benjaminwan@163.com>
2020-10-20 10:35:15 +0800

    修改测试图片文件夹名称，修改PC端项目测试脚本图片路径

--- pc_projects/OcrLiteOnnx/run-test-win.cmd
+++ pc_projects/OcrLiteOnnx/run-test-win.cmd
@@ -6,7 +6,7 @@ chcp 65001
 copy onnx\windows\onnxruntime.dll build-win /Y
 copy opencv\build\x64\vc14\bin\opencv_world3411.dll build-win /Y
 pushd build-win
-OcrLiteOnnx.exe ../../test/1.jpg ../models 0 0.6 0.3 3 1.3 1.3 1.6 1.6
+OcrLiteOnnx.exe ../../test_imgs/1.jpg ../models 0 0.6 0.3 3 1.3 1.3 1.6 1.6
 popd
 PAUSE
 @ENDLOCAL
--- pc_projects/OcrLiteOnnxToNcnn/run-test-win.cmd
+++ pc_projects/OcrLiteOnnxToNcnn/run-test-win.cmd
@@ -5,7 +5,7 @@ chcp 65001
 :: run Win x64
 copy opencv\build\x64\vc14\bin\opencv_world3411.dll build-win /Y
 pushd build-win
-OcrLiteOnnxToNcnn.exe ../../test/1.jpg ../models 0 0.6 0.3 3 1.3 1.3 1.6 1.6
+OcrLiteOnnxToNcnn.exe ../../test_imgs/1.jpg ../models 0 0.6 0.3 3 1.3 1.3 1.6 1.6
 popd
 PAUSE
 @ENDLOCAL
