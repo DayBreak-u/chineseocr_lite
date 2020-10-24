6f325998a17ae8af87c25710abe9518f2cdbf46e
benjaminwan <benjaminwan@163.com>
2020-10-24 20:23:23 +0800

    master分支ncnn_android_projects项目，修复文字方向检测错误，完成相册识别和摄像头识别页面，Jni识别过程信息回调到Jvm层显示

--- ncnn_android_projects/ChOcrLiteAndroidDBNet/OcrLibrary/src/main/cpp/main.cpp
+++ ncnn_android_projects/ChOcrLiteAndroidDBNet/OcrLibrary/src/main/cpp/main.cpp
@@ -1,41 +1,50 @@
-#include "ncnn/net.h"
-#include "LogUtils.h"
+#include "OcrResultUtils.h"
 #include "BitmapUtils.h"
 #include "OcrLite.h"
-#include "OcrUtil.h"
+#include "OcrUtils.h"
 
 static OcrLite *ocrLite;
 
 extern "C" JNIEXPORT jboolean JNICALL
-Java_com_benjaminwan_ocrlibrary_OcrEngine_init(JNIEnv *env, jobject thiz, jobject assetManager) {
+Java_com_benjaminwan_ocrlibrary_OcrEngine_init(JNIEnv *env, jobject thiz, jobject assetManager,
+                                               jint numThread) {
 
-    ocrLite = new OcrLite(env, assetManager);
+    ocrLite = new OcrLite(env, assetManager, numThread);
     return JNI_TRUE;
 }
 
+cv::Mat makePadding(cv::Mat &src, const int padding) {
+    if (padding <= 0) return src;
+    cv::Scalar paddingScalar = {255, 255, 255};
+    cv::Mat paddingSrc;
+    cv::copyMakeBorder(src, paddingSrc, padding, padding, padding, padding, cv::BORDER_ISOLATED,
+                       paddingScalar);
+    return paddingSrc;
+}
+
 extern "C"
-JNIEXPORT jstring JNICALL
+JNIEXPORT jobject JNICALL
 Java_com_benjaminwan_ocrlibrary_OcrEngine_detect(JNIEnv *env, jobject thiz, jobject input,
-                                                 jobject output, jint reSize,
+                                                 jobject output,
+                                                 jint padding, jint reSize,
                                                  jfloat boxScoreThresh, jfloat boxThresh,
-                                                 jfloat minArea, jfloat angleScaleWidth,
-                                                 jfloat angleScaleHeight,
-                                                 jfloat textScaleWidth, jfloat textScaleHeight) {
+                                                 jfloat minArea, jfloat scaleWidth,
+                                                 jfloat scaleHeight) {
+    LOGI("padding=%d,reSize=%d,boxScoreThresh=%f,boxThresh=%f,minArea=%f,scaleWidth=%f,scaleHeight=%f",
+         padding, reSize, boxScoreThresh, boxThresh, minArea, scaleWidth, scaleHeight);
     cv::Mat imgRGBA, imgBGR, imgOut;
     bitmapToMat(env, input, imgRGBA);
     cv::cvtColor(imgRGBA, imgBGR, cv::COLOR_RGBA2BGR);
-
+    cv::Rect originRect(padding, padding, imgBGR.cols, imgBGR.rows);
+    cv::Mat src = makePadding(imgBGR, padding);
     //按比例缩小图像，减少文字分割时间
-    ScaleParam s = getScaleParam(imgRGBA, reSize);//例：按长或宽缩放 src.cols=不缩放，src.cols/2=长度缩小一半
+    ScaleParam s = getScaleParam(src, reSize);//例：按长或宽缩放 src.cols=不缩放，src.cols/2=长度缩小一半
+    OcrResult ocrResult = ocrLite->detect(src, originRect, s,
+                                          boxScoreThresh, boxThresh, minArea,
+                                          scaleWidth, scaleHeight);
 
-    cv::Mat imgBox = imgBGR.clone();
-    std::string outStr = ocrLite->detect(imgBGR, s, imgBox,
-                                         boxScoreThresh, boxThresh, minArea,
-                                         angleScaleWidth, angleScaleHeight,
-                                         textScaleWidth, textScaleHeight);
-
-    cv::cvtColor(imgBox, imgOut, cv::COLOR_BGR2RGBA);
+    cv::cvtColor(ocrResult.boxImg, imgOut, cv::COLOR_BGR2RGBA);
     matToBitmap(env, imgOut, output);
 
-    return env->NewStringUTF(outStr.c_str());
+    return OcrResultUtils(env, ocrResult, output).getJObject();
 }
--- ncnn_android_projects/ChOcrLiteAndroidDBNet/OcrLibrary/src/main/java/com/benjaminwan/ocrlibrary/OcrEngine.kt
+++ ncnn_android_projects/ChOcrLiteAndroidDBNet/OcrLibrary/src/main/java/com/benjaminwan/ocrlibrary/OcrEngine.kt
@@ -5,35 +5,35 @@ import android.content.res.AssetManager
 import android.graphics.Bitmap
 
 class OcrEngine(context: Context) {
+    companion object {
+        const val numThread: Int = 4
+    }
+
     init {
         System.loadLibrary("OcrLite")
-        val ret = init(context.assets)
+        val ret = init(context.assets, numThread)
         if (!ret) throw IllegalArgumentException()
     }
 
+    var padding: Int = 0
     var boxScoreThresh: Float = 0.6f
     var boxThresh: Float = 0.3f
     var miniArea: Float = 3f
-    var angleScaleWidth: Float = 1.3f
-    var angleScaleHeight: Float = 1.3f
-    var textScaleWidth: Float = 1.6f
-    var textScaleHeight: Float = 1.6f
-
+    var scaleWidth: Float = 1.8f
+    var scaleHeight: Float = 1.8f
 
     fun detect(input: Bitmap, output: Bitmap, reSize: Int) =
         detect(
-            input, output, reSize,
+            input, output, padding, reSize,
             boxScoreThresh, boxThresh, miniArea,
-            angleScaleWidth, angleScaleHeight,
-            textScaleWidth, textScaleHeight
+            scaleWidth, scaleHeight
         )
 
-    external fun init(assetManager: AssetManager): Boolean
+    external fun init(assetManager: AssetManager, numThread: Int): Boolean
     external fun detect(
-        input: Bitmap, output: Bitmap, reSize: Int,
+        input: Bitmap, output: Bitmap, padding: Int, reSize: Int,
         boxScoreThresh: Float, boxThresh: Float, miniArea: Float,
-        angleScaleWidth: Float, angleScaleHeight: Float,
-        textScaleWidth: Float, textScaleHeight: Float
-    ): String
+        scaleWidth: Float, scaleHeight: Float
+    ): OcrResult
 
 }
