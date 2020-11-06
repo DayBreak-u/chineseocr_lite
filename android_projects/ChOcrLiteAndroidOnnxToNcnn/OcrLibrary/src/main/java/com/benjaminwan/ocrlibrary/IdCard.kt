0e732692afc45ff6c792610a11addcfaa63315fe
benjaminwan <benjaminwan@163.com>
2020-11-06 22:21:13 +0800

    Android Demo添加车牌识别和身份证识别页面

---
+++ android_projects/ChOcrLiteAndroidOnnxToNcnn/OcrLibrary/src/main/java/com/benjaminwan/ocrlibrary/IdCard.kt
@@ -0,0 +1,28 @@
+package com.benjaminwan.ocrlibrary
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class IdCard(
+    val name: String,//姓名
+    val nation: String,//民族
+    val address: String,//地址
+    val number: String,//身份证号
+) : Parcelable, OcrOutput() {
+    //性别
+    val gender: String
+        get() {
+            val genderNumber = number[16].toInt()
+            return if (genderNumber % 2 == 1) "男" else "女"
+        }
+
+    //出生日期
+    val birth: String
+        get() {
+            val year = number.substring(6..9)
+            val month = number.substring(10..11)
+            val date = number.substring(12..13)
+            return "$year-$month-$date"
+        }
+}
