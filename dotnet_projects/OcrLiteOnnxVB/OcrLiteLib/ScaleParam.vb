4e8d92afad0d34000628a685d84c508468f1eabb
benjaminwan <benjaminwan@163.com>
2020-12-05 23:23:51 +0800

    添加VB.Net项目及相关说明

---
+++ dotnet_projects/OcrLiteOnnxVB/OcrLiteLib/ScaleParam.vb
@@ -0,0 +1,103 @@
+﻿Imports Emgu.CV
+
+Namespace OcrLiteLib
+    Public Class ScaleParam
+        Public Property SrcWidth As Integer
+            Get
+                Return mSrcWidth
+            End Get
+            Set(value As Integer)
+                mSrcWidth = value
+            End Set
+        End Property
+        Private mSrcWidth As Integer
+        Public Property SrcHeight As Integer
+            Get
+                Return mSrcHeight
+            End Get
+            Set(value As Integer)
+                mSrcHeight = value
+            End Set
+        End Property
+        Private mSrcHeight As Integer
+
+        Public Property DstWidth As Integer
+            Get
+                Return mDstWidth
+            End Get
+            Set(value As Integer)
+                mDstWidth = value
+            End Set
+        End Property
+        Private mDstWidth As Integer
+        Public Property DstHeight As Integer
+            Get
+                Return mDstHeight
+            End Get
+            Set(value As Integer)
+                mDstHeight = value
+            End Set
+        End Property
+        Private mDstHeight As Integer
+        Public Property ScaleWidth As Single
+            Get
+                Return mScaleWidth
+            End Get
+            Set(value As Single)
+                mScaleWidth = value
+            End Set
+        End Property
+        Private mScaleWidth As Single
+        Public Property ScaleHeight As Single
+            Get
+                Return mScaleHeight
+            End Get
+            Set(value As Single)
+                mScaleHeight = value
+            End Set
+        End Property
+        Private mScaleHeight As Single
+        Public Sub New(ByVal srcWidth As Integer, ByVal srcHeight As Integer, ByVal dstWidth As Integer, ByVal dstHeight As Integer, ByVal scaleWidth As Single, ByVal scaleHeight As Single)
+            mSrcWidth = srcWidth
+            mSrcHeight = srcHeight
+            mDstWidth = dstWidth
+            mDstHeight = dstHeight
+            mScaleWidth = scaleWidth
+            mScaleHeight = scaleHeight
+        End Sub
+
+        Friend Shared Function GetScaleParam(src As Mat, dstSize As Integer) As ScaleParam
+            Dim SrcWidth As Integer
+            Dim SrcHeight As Integer
+            Dim DstWidth As Integer
+            Dim DstHeight As Integer
+
+            SrcWidth = src.Cols
+            DstWidth = src.Cols
+            SrcHeight = src.Rows
+            DstHeight = src.Rows
+
+            Dim scale As Single = 1.0F
+            If (DstWidth > DstHeight) Then
+                scale = CSng(dstSize) / CSng(DstWidth)
+                DstWidth = dstSize
+                DstHeight = CInt(CSng(DstHeight) * scale)
+            Else
+                scale = CSng(dstSize) / CSng(DstHeight)
+                DstHeight = dstSize
+                DstWidth = CInt(CSng(DstWidth) * scale)
+            End If
+            If (DstWidth Mod 32 <> 0) Then
+                DstWidth = (CInt(DstWidth / 32) - 1) * 32
+                DstWidth = Math.Max(DstWidth, 32)
+            End If
+            If (DstHeight Mod 32 <> 0) Then
+                DstHeight = (CInt(DstHeight / 32) - 1) * 32
+                DstHeight = Math.Max(DstHeight, 32)
+            End If
+            Dim ScaleWidth As Single = CSng(DstWidth) / CSng(SrcWidth)
+            Dim ScaleHeight As Single = CSng(DstHeight) / CSng(SrcHeight)
+            Return New ScaleParam(SrcWidth, SrcHeight, DstWidth, DstHeight, ScaleWidth, ScaleHeight)
+        End Function
+    End Class
+End Namespace
---
+++ dotnet_projects/OcrLiteOnnxVB/OcrLiteOnnxForm/FormOcrLite.vb
@@ -0,0 +1,64 @@
+﻿Imports System.IO
+Imports Emgu.CV
+Imports Emgu.CV.CvEnum
+Imports OcrLiteLib.OcrLiteLib
+
+Public Class FormOcrLite
+    Private ocrEngin As OcrLite
+    Private Sub FormOcrLite_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim appPath As String = AppDomain.CurrentDomain.BaseDirectory
+        Dim appDir As String = Directory.GetParent(appPath).FullName
+        Dim modelsDir As String = appPath & Convert.ToString("models")
+        modelsTextBox.Text = modelsDir
+        ocrEngin = New OcrLite(modelsDir, CInt(numThreadNumeric.Value))
+    End Sub
+
+    Private Sub initBtn_Click(sender As Object, e As EventArgs) Handles initBtn.Click
+        ocrEngin = New OcrLite(modelsTextBox.Text, CInt(numThreadNumeric.Value))
+    End Sub
+
+    Private Sub openBtn_Click(sender As Object, e As EventArgs) Handles openBtn.Click
+        Using dlg = New OpenFileDialog()
+            dlg.Multiselect = False
+            dlg.Filter = "(*.JPG,*.PNG,*.JPEG,*.BMP,*.GIF)|*.JPG;*.PNG;*.JPEG;*.BMP;*.GIF|All files(*.*)|*.*"
+            If dlg.ShowDialog() = DialogResult.OK AndAlso Not String.IsNullOrEmpty(dlg.FileName) Then
+                pathTextBox.Text = dlg.FileName
+                Dim src As Mat = CvInvoke.Imread(dlg.FileName, ImreadModes.Color)
+                pictureBox.Image = src.ToBitmap()
+            End If
+        End Using
+    End Sub
+
+    Private Sub modelsBtn_Click(sender As Object, e As EventArgs) Handles modelsBtn.Click
+        Using dlg = New FolderBrowserDialog()
+            dlg.SelectedPath = Environment.CurrentDirectory + "\models"
+            If dlg.ShowDialog() = DialogResult.OK AndAlso Not String.IsNullOrEmpty(dlg.SelectedPath) Then
+                modelsTextBox.Text = dlg.SelectedPath
+            End If
+        End Using
+    End Sub
+
+    Private Sub detectBtn_Click(sender As Object, e As EventArgs) Handles detectBtn.Click
+        Dim padding As Integer = CInt(paddingNumeric.Value)
+        Dim imgResize As Integer = CInt(imgResizeNumeric.Value)
+        Dim boxScoreThresh As Single = CSng(boxScoreThreshNumeric.Value)
+        Dim boxThresh As Single = CSng(boxThreshNumeric.Value)
+        Dim minArea As Single = CSng(minAreaNumeric.Value)
+        Dim unClipRatio As Single = CSng(unClipRatioNumeric.Value)
+        Dim doAngle As Boolean = doAngleCheckBox.Checked
+        Dim mostAngle As Boolean = mostAngleCheckBox.Checked
+        Dim ocrResult As OcrResult = ocrEngin.Detect(pathTextBox.Text, padding, imgResize, boxScoreThresh, boxThresh, minArea,
+                unClipRatio, doAngle, mostAngle)
+        ocrResultTextBox.Text = ocrResult.ToString()
+        strRestTextBox.Text = ocrResult.StrRes
+        pictureBox.Image = ocrResult.BoxImg.ToBitmap()
+    End Sub
+
+    Private Sub partImgCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles partImgCheckBox.CheckedChanged
+        ocrEngin.isPartImg = partImgCheckBox.Checked
+    End Sub
+
+    Private Sub debugCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles debugCheckBox.CheckedChanged
+        ocrEngin.isDebugImg = debugCheckBox.Checked
+    End Sub
+End Class
