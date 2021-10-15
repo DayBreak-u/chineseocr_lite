a5261c69350e76f8ec1dfd8ee942bf709ef83d8d
benjaminwan <benjaminwan@163.com>
2021-10-15 10:23:31 +0800

    OcrLiteNcnn项目升级1.6.0

--- cpp_projects/OcrLiteNcnn/build.bat
+++ cpp_projects/OcrLiteNcnn/build.bat
@@ -7,111 +7,65 @@ echo.
 
 echo "========编译选项========"
 echo "请注意：项目默认使用Release库，除非您自行编译Debug版的ncnn和Opencv，否则请不要选择Debug编译"
-echo "请输入编译选项并回车: 1)Release, 2)Debug, 3)预设"
+echo "请输入编译选项并回车: 1)Release, 2)Debug"
 set BUILD_TYPE=Release
 set /p flag=
 if %flag% == 1 (set BUILD_TYPE=Release)^
 else if %flag% == 2 (set BUILD_TYPE=Debug)^
-else if %flag% == 3 (goto :makeAllExe)^
 else (echo 输入错误！Input Error!)
-echo.
-
-echo "请输入OpenMP选项并回车: 1)启用OpenMP(Angle阶段和Crnn阶段多线程并行执行), 2)禁用OpenMP(Angle阶段和Crnn阶段单线程执行)"
-set BUILD_OPENMP=ON
-set /p flag=
-if %flag% == 1 (set BUILD_OPENMP=ON)^
-else if %flag% == 2 (set BUILD_OPENMP=OFF)^
-else (echo 输入错误！Input Error!)
-echo.
-
-echo "使用静态库时，编译出来的可执行文件较大，但部署起来比较方便。"
-echo "使用动态库时，编译出来的可执行文件较小，但部署的时候记得把dll复制到可执行文件目录"
-echo "请选择要使用的Opencv库选项并回车: 1)Static静态库，2)Shared动态库"
-set BUILD_STATIC=ON
-set /p flag=
-if %flag% == 1 (set BUILD_STATIC=ON)^
-else if %flag% == 2 (set BUILD_STATIC=OFF)^
-else (echo "输入错误！Input Error!")
 echo.
 
 echo "请选择要使用的ncnn库选项并回车: 1)ncnn(CPU)，2)ncnn(vulkan)"
-set BUILD_NCNN_VULKAN=OFF
+set BUILD_NCNN_VULKAN="CPU"
 set /p flag=
-if %flag% == 1 (set BUILD_NCNN_VULKAN=OFF)^
-else if %flag% == 2 (set BUILD_NCNN_VULKAN=ON)^
+if %flag% == 1 (set BUILD_NCNN_VULKAN="CPU")^
+else if %flag% == 2 (set BUILD_NCNN_VULKAN="GPU")^
 else (echo "输入错误！Input Error!")
 echo.
 
-echo "请注意：如果选择2)编译为JNI动态库时，必须安装配置Oracle JDK"
-echo "请选择编译输出类型并回车: 1)编译成可执行文件，2)编译成JNI动态库，3)编译成C层动态库"
-set BUILD_JNI=OFF
-set BUILD_CLIB=OFF
+echo "请注意：如果选择2)JNI动态库时，必须安装配置Oracle JDK"
+echo "请选择编译输出类型并回车: 1)BIN可执行文件，2)JNI动态库，3)C动态库"
+set BUILD_OUTPUT="EXE"
 set /p flag=
-if %flag% == 1 (set BUILD_JNI=OFF)^
-else if %flag% == 2 (set BUILD_JNI=ON)^
-else if %flag% == 3 (set BUILD_CLIB=ON)^
+if %flag% == 1 (set BUILD_OUTPUT="BIN")^
+else if %flag% == 2 (set BUILD_OUTPUT="JNI")^
+else if %flag% == 3 (set BUILD_OUTPUT="CLIB")^
 else (echo 输入错误！Input Error!)
 echo.
 
-if %BUILD_JNI% == ON (call :makeJni)^
-else if %BUILD_CLIB% == ON (call :makeCLib)^
-else (call :makeExe)
-echo cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DOCR_OPENMP=%BUILD_OPENMP% -DOCR_JNI=%BUILD_JNI% -DOCR_CLIB=%BUILD_CLIB% -DOCR_STATIC=%BUILD_STATIC% -DOCR_VULKAN=%BUILD_NCNN_VULKAN% ..
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DOCR_OPENMP=%BUILD_OPENMP% -DOCR_JNI=%BUILD_JNI% -DOCR_CLIB=%BUILD_CLIB% -DOCR_STATIC=%BUILD_STATIC% -DOCR_VULKAN=%BUILD_NCNN_VULKAN% ..
-nmake
+echo "VS版本: 1)vs2017-x64，2)vs2017-x86, 3)vs2019-x64, 4)vs2019-x86"
+set BUILD_CMAKE_T="v141"
+set BUILD_CMAKE_A="x64"
+set /p flag=
+if %flag% == 1 (
+    set BUILD_CMAKE_T="v141"
+    set BUILD_CMAKE_A="x64"
+)^
+else if %flag% == 2 (
+    set BUILD_CMAKE_T="v141"
+    set BUILD_CMAKE_A="Win32"
+)^
+else if %flag% == 3 (
+    set BUILD_CMAKE_T="v142"
+    set BUILD_CMAKE_A="x64"
+)^
+else if %flag% == 4 (
+    set BUILD_CMAKE_T="v142"
+    set BUILD_CMAKE_A="Win32"
+)^
+else (echo 输入错误！Input Error!)
+echo.
+
+mkdir win-%BUILD_OUTPUT%-%BUILD_NCNN_VULKAN%-%BUILD_CMAKE_A%
+pushd win-%BUILD_OUTPUT%-%BUILD_NCNN_VULKAN%-%BUILD_CMAKE_A%
+
+cmake -T "%BUILD_CMAKE_T%,host=x64" -A %BUILD_CMAKE_A% ^
+  -DCMAKE_INSTALL_PREFIX=install ^
+  -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DOCR_OUTPUT=%BUILD_OUTPUT% -DOCR_VULKAN=%BUILD_NCNN_VULKAN% ..
+cmake --build . --config %BUILD_TYPE% -j %NUMBER_OF_PROCESSORS%
+cmake --build . --config %BUILD_TYPE% --target install
+
 popd
 GOTO:EOF
-
-:makeExe
-mkdir build
-pushd build
-GOTO:EOF
-
-:makeJni
-mkdir build-jni
-pushd build-jni
-GOTO:EOF
-
-:makeCLib
-mkdir build-clib
-pushd build-clib
-GOTO:EOF
-
-:makeAllExe
-mkdir win-cpu-%VSCMD_ARG_TGT_ARCH%
-pushd win-cpu-%VSCMD_ARG_TGT_ARCH%
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DOCR_OPENMP=OFF -DOCR_JNI=OFF -DOCR_CLIB=OFF -DOCR_STATIC=ON -DOCR_VULKAN=OFF ..
-nmake
-popd
-
-mkdir win-gpu-%VSCMD_ARG_TGT_ARCH%
-pushd win-gpu-%VSCMD_ARG_TGT_ARCH%
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DOCR_OPENMP=OFF -DOCR_JNI=OFF -DOCR_CLIB=OFF -DOCR_STATIC=ON -DOCR_VULKAN=ON ..
-nmake
-popd
-
-mkdir win-jni-cpu-%VSCMD_ARG_TGT_ARCH%
-pushd win-jni-cpu-%VSCMD_ARG_TGT_ARCH%
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DOCR_OPENMP=OFF -DOCR_JNI=ON -DOCR_CLIB=OFF -DOCR_STATIC=ON -DOCR_VULKAN=OFF ..
-nmake
-popd
-
-mkdir win-jni-gpu-%VSCMD_ARG_TGT_ARCH%
-pushd win-jni-gpu-%VSCMD_ARG_TGT_ARCH%
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DOCR_OPENMP=OFF -DOCR_JNI=ON -DOCR_CLIB=OFF -DOCR_STATIC=ON -DOCR_VULKAN=ON ..
-nmake
-popd
-
-mkdir win-clib-cpu-%VSCMD_ARG_TGT_ARCH%
-pushd win-clib-cpu-%VSCMD_ARG_TGT_ARCH%
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DOCR_OPENMP=OFF -DOCR_JNI=OFF -DOCR_CLIB=ON -DOCR_STATIC=ON -DOCR_VULKAN=OFF ..
-nmake
-popd
-
-mkdir win-clib-gpu-%VSCMD_ARG_TGT_ARCH%
-pushd win-clib-gpu-%VSCMD_ARG_TGT_ARCH%
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DOCR_OPENMP=OFF -DOCR_JNI=OFF -DOCR_CLIB=ON -DOCR_STATIC=ON -DOCR_VULKAN=ON ..
-nmake
-popd
 
 @ENDLOCAL
--- cpp_projects/OcrLiteNcnn/run-benchmark.sh
+++ cpp_projects/OcrLiteNcnn/run-benchmark.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+function PrepareVar(){
+echo "Gpu版本测试前请先安装Vulkan SDK v1.2.162.0，https://vulkan.lunarg.com/sdk/home"
+echo "请输入测试选项并回车: 1)CPU, 2)GPU"
+read -p "" RUN_ARCH
+if [ $RUN_ARCH == 1 ]; then
+    EXE_PATH=${sysOS}-BIN-CPU
+    GPU_INDEX=-1
+elif [ $RUN_ARCH == 2 ]; then
+    EXE_PATH=${sysOS}-BIN-GPU
+    GPU_INDEX=0
+else
+  echo -e "输入错误！Input Error!"
+fi
+}
+
 sysOS=`uname -s`
 NUM_THREADS=1
 if [ $sysOS == "Darwin" ];then
@@ -15,18 +30,19 @@ fi
 echo "Setting the Number of Threads=$NUM_THREADS Using an OpenMP Environment Variable"
 set OMP_NUM_THREADS=$NUM_THREADS
 
-echo "请输入循环次数:"
+PrepareVar
+
+echo "请输入循环次数"
 read -p "" LOOP_COUNT
 
-TARGET_IMG=../images/1.jpg
+TARGET_IMG=images/1.jpg
 if [ ! -f "$TARGET_IMG" ]; then
 echo "找不到待识别的目标图片：${TARGET_IMG}，请打开本文件并编辑TARGET_IMG"
 exit
 fi
 
-##### run test on MacOS or Linux
-./build/benchmark --version
-./build/benchmark --models models \
+./${EXE_PATH}/benchmark --version
+./${EXE_PATH}/benchmark --models models \
 --det dbnet_op \
 --cls angle_op \
 --rec crnn_lite_op \
@@ -34,10 +50,11 @@ fi
 --image $TARGET_IMG \
 --numThread $NUM_THREADS \
 --padding 50 \
---maxSideLen 1024 \
+--maxSideLen 0 \
 --boxScoreThresh 0.6 \
 --boxThresh 0.3 \
 --unClipRatio 2.0 \
 --doAngle 1 \
 --mostAngle 1 \
---loopCount $LOOP_COUNT -G 0
+--GPU $GPU_INDEX \
+--loopCount $LOOP_COUNT
